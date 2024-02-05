df693ce0a8d44047d97b6ea5a365c51939f57e17
Simon Krempin <67335158+simonkrempin@users.noreply.github.com>
2024-02-05 18:45:42 +0100

    creating project structure

---
+++ lib/pages/connected.dart
@@ -0,0 +1,15 @@
+import "package:flutter/material.dart";
+
+class Connected extends StatefulWidget {
+  const Connected({Key? key}) : super(key: key);
+
+  @override
+  State<Connected> createState() => _ConnectedState();
+}
+
+class _ConnectedState extends State<Connected> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
---
+++ lib/pages/disconnected.dart
@@ -0,0 +1,25 @@
+import "package:flutter/material.dart";
+
+class Disconnected extends StatefulWidget {
+  const Disconnected({Key? key}) : super(key: key);
+
+  @override
+  State<Disconnected> createState() => _DisconnectedState();
+}
+
+class _DisconnectedState extends State<Disconnected> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Icon(Icons.bluetooth_rounded, size: 100),
+            Text("Verbinde dein Gerät", style: TextStyle(fontSize: 20)),
+          ],
+        ),
+      ),
+    );
+  }
+}
